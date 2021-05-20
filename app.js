e8745880857fadaf16221b5b34ea885eb160705d
yitzhak-malik <izakmalik@gmail.com>
2021-05-20 17:51:06 +0300

    'first'

---
+++ app.js
@@ -0,0 +1,5 @@
+const port=process.env.PORT||8080
+var exspress =require("express");
+var app=exspress();
+app.listen(port);
+app.use('/api/users',)
---
+++ controls/userControl.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const usesrControllers=express.Router();
+usesrControllers.get('')
