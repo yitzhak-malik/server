7f7bea32743a09eb34b2763e23db65856575c75b
yitzhak-malik <izakmalik@gmail.com>
2021-06-21 18:21:38 +0300

    populate

--- schema/supervisorsSchema.js
+++ schema/supervisorsSchema.js
@@ -2,8 +2,9 @@ const mongoose=require('mongoose');
 const schema=mongoose.Schema;
 
 var supervisorSchema=new schema({
-    academics:{type:mongoose.Types.ObjectId,ref:' academic'},
-    interns:{type:mongoose.Types.ObjectId,ref:'user'}
+    academics:[{type:mongoose.Types.ObjectId,ref:' academic'}],
+    interns:[{type:mongoose.Types.ObjectId,ref:'user'}],
+    class:[{type:mongoose.Types.ObjectId,ref:'class'}],
   
 
 })
